cb777fe25d76f976b24bd418db28c9e932071f72
angrysine <betorm20022@gmail.com>
2024-10-06 16:57:37 -0300

    gitignore

--- neural_nertwork_implementation/target/debug/deps/neural_nertwork_implementation-d21814bff88938ba.d
+++
@@ -1,5 +0,0 @@
-/home/albertomiranda/Documents/github/PonderadasMod11/neural_nertwork_implementation/target/debug/deps/libneural_nertwork_implementation-d21814bff88938ba.rmeta: src/main.rs
-
-/home/albertomiranda/Documents/github/PonderadasMod11/neural_nertwork_implementation/target/debug/deps/neural_nertwork_implementation-d21814bff88938ba.d: src/main.rs
-
-src/main.rs:
--- neural_nertwork_implementation/target/debug/deps/neural_nertwork_implementation-d3cb608560d3d626.d
+++
@@ -1,9 +0,0 @@
-/home/albertomiranda/Documents/github/PonderadasMod11/neural_nertwork_implementation/target/debug/deps/libneural_nertwork_implementation-d3cb608560d3d626.rmeta: src/lib.rs src/tensor/mod.rs src/tensor/tensor.rs src/layer/mod.rs src/layer/layer.rs
-
-/home/albertomiranda/Documents/github/PonderadasMod11/neural_nertwork_implementation/target/debug/deps/neural_nertwork_implementation-d3cb608560d3d626.d: src/lib.rs src/tensor/mod.rs src/tensor/tensor.rs src/layer/mod.rs src/layer/layer.rs
-
-src/lib.rs:
-src/tensor/mod.rs:
-src/tensor/tensor.rs:
-src/layer/mod.rs:
-src/layer/layer.rs:
